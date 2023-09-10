b650cf1e6bd326298e87ea48e7f7c2ee2e554c95
lmaddox <InnovAnon-Inc@protonmail.com>
2023-09-09 19:36:44 -0500

    update

--- src/teamhack_dns/server.py
+++ src/teamhack_dns/server.py
@@ -1,12 +1,9 @@
-import socket
+from socket          import socket AF_INET, SOCK_DGRAM
 from dnslib          import *
 from teamhack_db.sql import select_hostname_recordtype
 
-UPSTREAM_SERVER = '8.8.8.8'
-UPSTREAM_PORT   = 53
-
 # Function to handle DNS queries and return a response
-def handle_dns_query(conn, data):
+def handle_dns_query(conn, data, upstream_server, upstream_port):
     request = DNSRecord.parse(data)
 
     reply = DNSRecord(DNSHeader(id=request.header.id, qr=1, aa=1, ra=1), q=request.q)
@@ -16,7 +13,7 @@ def handle_dns_query(conn, data):
     #if   qtype == QTYPE.NS: qt = 'NS'
     #elif qtype == QTYPE.A:  qt = 'A'
     #else:
-    #  a = request.send(UPSTREAM_SERVER, UPSTREAM_PORT, tcp=False, timeout=10)
+    #  a = request.send(upstream_server, upstream_port, tcp=False, timeout=10)
     #  request.add_answer(a)
     #  return request.pack()
 
@@ -24,16 +21,16 @@ def handle_dns_query(conn, data):
     res = select_hostname_recordtype(conn, qname, qtype)
     print(f'res: {res}')
     if not res:
-      a = request.send(UPSTREAM_SERVER, UPSTREAM_PORT, tcp=False, timeout=10)
+      a = request.send(upstream_server, upstream_port, tcp=False, timeout=10)
       return a
     res = res[3]
     print(f'res: {res}')
     if not res:
-      a = request.send(UPSTREAM_SERVER, UPSTREAM_PORT, tcp=False, timeout=10)
+      a = request.send(upstream_server, upstream_port, tcp=False, timeout=10)
       return a
     #res = res[3]
     #if not res:
-    #  a = request.send(UPSTREAM_SERVER, UPSTREAM_PORT, tcp=False, timeout=10)
+    #  a = request.send(upstream_server, upstream_port, tcp=False, timeout=10)
     #  return a
     print(f'qname: {qname}, qtype: {qtype}, res: {res}')
 
@@ -45,16 +42,13 @@ def handle_dns_query(conn, data):
     #    # TODO
     #    #reply.add_answer(RR(rname=qname, rtype=qtype, rdata=A('0.0.0.0')))
     #    #q = DNSRecord(q=DNSQuestion(qname))
-    #    a = reply.send(UPSTREAM_SERVER, UPSTREAM_PORT, tcp=False, timeout=10)
+    #    a = reply.send(upstream_server, upstream_port, tcp=False, timeout=10)
     #    reply.add_answer(a)
 
     return reply.pack()
 
 # Function to start the DNS server and listen for requests
-def start_server(conn):
-    host = ''
-    port = 53
-
+def start_server(conn, host='', port=53, upstream_server='8.8.8.8', upstream_port=53):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind((host, port))
 
@@ -62,6 +56,6 @@ def start_server(conn):
 
     while True:
         data, address = server_socket.recvfrom(1024)
-        response = handle_dns_query(conn, data)
+        response = handle_dns_query(conn, data, upstream_server, upstream_port)
         server_socket.sendto(response, address)
 
