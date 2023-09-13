16ba67eb877d72c2e22176dcf9989167e025104b
lmaddox <InnovAnon-Inc@protonmail.com>
2023-09-12 23:49:22 -0500

    update

--- src/import_db/__main__.py
+++ src/import_db/__main__.py
@@ -2,9 +2,9 @@ from os                 import environ
 from            .server import start_server
 
 if __name__ == '__main__':
-  password  = environ['MSFRPC_PASSWORD']
-  username  = environ['MSFRPC_USERNAME']
-  upstreamh = environ['MSFRPC_HOSTNAME']
+  password  = environ.get('MSFRPC_PASSWORD')
+  username  = environ.get('MSFRPC_USERNAME')
+  upstreamh = environ.get('MSFRPC_HOSTNAME')
   start_server(password, username, upstreamh)
 
 
